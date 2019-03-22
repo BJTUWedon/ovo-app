bda2897424e3e20fdd0a43bd54780d250aa146e6
Wedon <604543216@qq.com>
2019-03-22 09:45:57 -0400

    1

--- OvO/settings.py
+++ OvO/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.path.join(os.path.dirname(__file__) ,'../templates').replace('\\','/')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
